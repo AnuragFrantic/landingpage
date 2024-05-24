47e0fbe6e55d268300f1432a65a6df53a7c7eec2
Anuragpandey001 <ap1663392@gmail.com>
2024-05-24 19:34:50 +0530

    checking

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,28 +11,31 @@ function Home() {
             <nav class="navbar navbar-expand-lg bg-dark">
                 <div class="container-fluid">
                     <div className="">
-                        <a class="navbar-brand" href="#">
-                            <img src={logo} className='img-fluid' style={{ height: "80px" }} alt="" />
+                        <a class="navbar-brand logo" href="#">
+                            <img src={logo} className='img-fluid' alt="" />
                         </a>
 
                     </div>
                     <div className="">
-                        <a class="navbar-brand" href="#">
-                            <span className='text-white me-3'> GET ID NOW</span>
-                            <img src={whatsapp} className='img-fluid' style={{ height: "40px" }} alt="" />
+                        <a class="navbar-brand headerimg" href='https://wa.me/+917817078742' target='_blank'>
+                            <span className='text-white me-1'> GET ID NOW</span>
+                            <img src={whatsapp} className='img-fluid' alt="" />
+                        </a>
+                        <a class="navbar-brand headerimg" href='https://t.me/linkw999' target='_blank'>
+                            <span className='text-white me-1'> GET ID NOW</span>
+                            <img src={telegram} style={{ borderRadius: "50%" }} className='img-fluid' alt="" />
                         </a>
                     </div>
-
-
-
-
 
                 </div>
             </nav>
 
             <section className='mainsection'>
-                <a href='https://wa.me/+917817078742' target='_blank' className='btn shadow-none whatsappicon' id="blinking-button">GET ID NOW <img src={whatsapp} style={{ height: "30px" }} alt="" /></a>
-                <a href='https://t.me/linkw999' className='btn shadow-none telegramicon' id="blinking-button">GET ID NOW <img src={telegram} style={{ height: "30px", borderRadius: "50%" }} alt="" /></a>
+                <div className="buttonparent position-absolute d-flex w-100 justify-content-between">
+                    <a href='https://wa.me/+917817078742' target='_blank' className='btn shadow-none whatsappicon' id="blinking-button">GET ID NOW <img src={whatsapp} alt="" /></a>
+                    <a href='https://t.me/linkw999' target='_blank' className='btn shadow-none telegramicon' id="blinking-button">GET ID NOW <img src={telegram} alt="" /></a>
+                </div>
+
             </section>
         </>
     )
